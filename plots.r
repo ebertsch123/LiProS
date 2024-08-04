4f31c5b7f5913ea39045f076db05b5bd86804aaf
ebertsch123 <82252227+ebertsch123@users.noreply.github.com>
2024-08-04 14:15:58 -0600

    Add files via upload

---
+++ plots.r
@@ -0,0 +1,164 @@
+source('models.r')
+
+
+library(ggplot2)
+library(ggcorrplot)
+library(gridExtra)
+
+
+#correlation plots of the descriptors after the feature selection
+corr <- round(cor(acids_d[,-ncol(acids_d)]),2)
+p <- ggcorrplot(corr,show.diag = F,type='lower', lab = TRUE, lab_size=3) + 
+  scale_fill_gradient2(low='blue4',high='red4',breaks=c(-1,-0.5,0,0.5,1),limit=c(-1,1),name='Correlation')
+ggsave(p,file='plots/corr_A.pdf',width = 4000, height = 3734,units = 'px')
+ggsave(p,file='plots/corr_A.png',width = 4000, height = 3734,units = 'px')
+
+
+
+corr <- round(cor(bases_d[,-ncol(bases_d)]),2)
+p <- ggcorrplot(corr,show.diag = F,type='lower', lab = TRUE, lab_size=3) + 
+  scale_fill_gradient2(low='blue4',high='red4',breaks=c(-1,-0.5,0,0.5,1),limit=c(-1,1),name='Correlation')
+ggsave(p,file='plots/corr_B.pdf',width = 4000, height = 3734,units = 'px')
+ggsave(p,file='plots/corr_B.png',width = 4000, height = 3734,units = 'px')
+
+
+
+
+
+#violin plots for acid and basic descriptors that had p < 0.05
+
+acids_violins <- list() #save every plot in this list
+
+#automate plotting into the list
+for (i in 1:(ncol(acids_d)-1)) {
+  col_name <- colnames(acids_d)[i]
+  
+  p <- ggplot(acids_d) + 
+    geom_violin(aes_string(x = "cond", y = col_name),fill='gray',color="black", size=0.4, alpha=0.5) +
+    geom_boxplot(aes_string(x = "cond", y = col_name, fill = "cond"),width=0.8) +
+    theme(panel.background = element_blank(), panel.border = element_rect(fill=NA,colour="black",size=1),
+          axis.title.x = element_text(size = 10),
+          axis.text.x = element_text(size = 8),
+          axis.text.y = element_text(size = 8),
+          axis.title.y = element_text(size = 10),
+          legend.title = element_text(size=10),
+          legend.text = element_text(size=8),
+          legend.position = 'none') + 
+    labs(y=col_name,x='Better Fit') +
+    scale_fill_manual(name="Better fit",
+                      values=c("seagreen4", "red"),
+                      labels=c('Eq. 2','Both or Eq. 1')) + 
+    scale_x_discrete(labels=c("0" = "Eq. 2", "1" = "Both or Eq. 1"))
+  
+  acids_violins[[i]] <- p #se pone cada grafica en la lista
+}
+
+
+
+ncol <- 5
+nrow <- 5
+
+#grid plot creation
+grid_acids <- grid.arrange(do.call(arrangeGrob, c(acids_violins,ncol = ncol, nrow = nrow)))
+ggsave('plots/acids_violins.pdf', plot = grid_acids, width = 11, height = 11, units = 'in')
+ggsave('plots/acids_violins.png', plot = grid_acids, width = 11, height = 11, units = 'in')
+
+
+
+
+
+
+bases_violins <- list() #save every plot in this list
+
+#automate plotting into the list
+for (i in 1:(ncol(bases_d)-1)) {
+  col_name <- colnames(bases_d)[i]
+  
+  p <- ggplot(bases_d) + 
+    geom_violin(aes_string(x = "cond", y = col_name),fill='gray',color="black", size=0.4, alpha=0.5) +
+    geom_boxplot(aes_string(x = "cond", y = col_name, fill = "cond"),width=0.8) +
+    theme(panel.background = element_blank(), panel.border = element_rect(fill=NA,colour="black",size=1),
+          axis.title.x = element_text(size = 10),
+          axis.text.x = element_text(size = 8),
+          axis.text.y = element_text(size = 8),
+          axis.title.y = element_text(size = 10),
+          legend.title = element_text(size=10),
+          legend.text = element_text(size=8),
+          legend.position = 'none') + 
+    labs(y=col_name,x='Better Fit') +
+    scale_fill_manual(name="Better fit",
+                      values=c("seagreen4", "red"),
+                      labels=c('Eq. 2','Both or Eq. 1')) + 
+    scale_x_discrete(labels=c("0" = "Eq. 2", "1" = "Both or Eq. 1"))
+  
+  bases_violins[[i]] <- p #se pone cada grafica en la lista
+}
+
+
+
+ncol <- 5
+nrow <- 7
+
+#grid plot creation
+grid_bases <- grid.arrange(do.call(arrangeGrob, c(bases_violins,ncol = ncol, nrow = nrow)))
+ggsave('plots/bases_violins.pdf', plot = grid_bases, width = 11, height = 11, units = 'in')
+ggsave('plots/bases_violins.png', plot = grid_bases, width = 11, height = 11, units = 'in')
+
+
+
+
+
+
+
+#descriptor importance plot (from Random Forest models)
+
+#acids
+
+ImpData_A <- as.data.frame(importance(model_RF_A))
+ImpData_A$Var.Names <- row.names(ImpData_A)
+
+p1 <- ggplot(ImpData_A, aes(x=Var.Names, y=MeanDecreaseGini)) +
+  labs(x="", y = "MeanDecreaseGini") + 
+  geom_segment( aes(x=Var.Names, xend=Var.Names, y=0, yend=MeanDecreaseGini), color="gray") +
+  geom_point(aes(size = MeanDecreaseGini), color="blue4", alpha=0.8) +
+  coord_flip() +
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(fill=NA,colour="black",size=1),
+        axis.title.x = element_text(size = 16),
+        axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.y = element_text(size = 16),
+        legend.title = element_text(size=16),
+        legend.text = element_text(size=14),
+        legend.position='none')
+
+
+
+#bases
+ImpData_B <- as.data.frame(importance(model_RF_B))
+ImpData_B$Var.Names <- row.names(ImpData_B)
+
+p2 <- ggplot(ImpData_B, aes(x=Var.Names, y=MeanDecreaseGini)) +
+  labs(x="", y = "MeanDecreaseGini") + 
+  geom_segment( aes(x=Var.Names, xend=Var.Names, y=0, yend=MeanDecreaseGini), color="gray") +
+  geom_point(aes(size = MeanDecreaseGini), color="red4", alpha=0.8) +
+  coord_flip() +
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(fill=NA,colour="black",size=1),
+        axis.title.x = element_text(size = 16),
+        axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.y = element_text(size = 16),
+        legend.title = element_text(size=16),
+        legend.text = element_text(size=14),
+        legend.position='none')
+
+
+
+plots <- list(p1,p2)
+
+
+plots_grid <- grid.arrange(do.call(arrangeGrob,c(plots, ncol = 2, nrow = 1)))
+
+ggsave('plots/importance.pdf', plot = plots_grid, width = 15, height = 6, units = 'in')
+ggsave('plots/importance.png', plot = plots_grid, width = 15, height = 6, units = 'in')
